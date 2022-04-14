87c8de318d75e80e392aa432e09503b5a3367e7e
Wildiney Di Masi <wildiney@gmail.com>
2022-04-14 15:53:22 -0300

    update code

--- config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "root";
-$mysql_database = "appOlimpiadas";
---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("button#inputCadastrar").attr("disabled", true);
+    $("#inputAceite").on("change", function () {
+        if ($("#inputAceite").is(':checked')) {
+            console.log("checked");
+            $("button#inputCadastrar").attr("disabled", false);
+        } else {
+            console.log("unchecked");
+            $("button#inputCadastrar").attr("disabled", true);
+        }
+    });
+});
